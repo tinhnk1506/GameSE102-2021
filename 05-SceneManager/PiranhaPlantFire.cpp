3715ba32d048ceedc661f7cd33902fa3563f64c6
tinhnk1506 <tinhnk1506@gmail.com>
2021-11-22 20:46:15 +0700

    merge

---
+++ 05-SceneManager/PiranhaPlantFire.cpp
@@ -0,0 +1,173 @@
+#include "PiranhaPlantFire.h"
+#include "Mario.h"
+#include "FireBullet.h"
+#include "PlayScene.h"
+
+PiranhaPlantFire::PiranhaPlantFire(int tag) {
+	this->tag = tag;
+	SetState(PIRANHAPLANT_STATE_INACTIVE);
+}
+
+void PiranhaPlantFire::Render()
+{
+	int ani = PIRANHAPLANT_ANI_DEATH;
+	if (state != PIRANHAPLANT_STATE_DEATH && dying_start == 0)
+	{
+		if (Up)
+			if (Right)
+				ani = PIRANHAPLANT_ANI_RIGHT_UP;
+			else
+				ani = PIRANHAPLANT_ANI_LEFT_UP;
+		else
+			if (Right)
+				ani = PIRANHAPLANT_ANI_RIGHT_DOWN;
+			else
+				ani = PIRANHAPLANT_ANI_LEFT_DOWN;
+	}
+	animation_set->at(ani)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void PiranhaPlantFire::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
+
+	if (GetTickCount64() - dying_start >= PIRANHAPLANT_DIYING_TIME && dying_start != 0)
+		isDeleted = true;
+
+	if (state == PIRANHAPLANT_STATE_DEATH)
+		return;
+
+	CGameObject::Update(dt);
+	y += vy * dt;
+
+	GetDirect();
+
+	if (y <= limitY && vy < 0)
+	{
+		y = limitY;
+		vy = 0;
+		StartAim();
+	}
+
+	if (y >= limitY + BBHeight && vy > 0)
+	{
+		y = limitY + BBHeight + 12;
+		SetState(PIRANHAPLANT_STATE_INACTIVE);
+		StartDelay();
+	}
+	/*int res = GetTickCount64() - aim_start >= PIRANHAPLANT_AIM_TIME && aim_start != 0 ? 1 : 0;
+	DebugOut(L"[FirePiran_aim_start]::%d\n", aim_start);
+	DebugOut(L"[GetTickCount64()]::%d\n", GetTickCount64());
+	DebugOut(L"[res]::%d\n", res);*/
+	if (GetTickCount64() - aim_start >= PIRANHAPLANT_AIM_TIME && aim_start != 0)
+	{
+		aim_start = 0;
+		SetState(PIRANHAPLANT_STATE_SHOOTING);
+		StartDelay();
+	}
+
+	if (GetTickCount64() - delay_start >= PIRANHAPLANT_DELAY_TIME && delay_start != 0)
+	{
+		delay_start = 0;
+		if (y == limitY)
+			vy = PIRANHAPLANT_DARTING_SPEED;
+	}
+
+	CMario* mario = ((CPlayScene*)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	if (mario != NULL) {
+		float mLeft, mTop, mRight, mBottom;
+		float oLeft, oTop, oRight, oBottom;
+
+		int mWidth = mario->GetWidth();
+		/*if ((floor(mario->x) + (float)mWidth + PIRANHAPLANT_ACTIVE_RANGE <= x
+			|| ceil(mario->x) >= x + PIRANHAPLANT_BBOX_WIDTH + PIRANHAPLANT_ACTIVE_RANGE)
+			&& state == PIRANHAPLANT_STATE_INACTIVE && delay_start == 0)
+			SetState(PIRANHAPLANT_STATE_DARTING);*/
+			//! 60 is red piranha active zone
+		if (x - 60 <= floor(mario->x)
+			&& floor(mario->x) + (float)mWidth <= x
+			&& state == PIRANHAPLANT_STATE_INACTIVE && delay_start == 0
+			&& !isMarioInActiveZone
+			|| floor(mario->x) > x && floor(mario->x) < x + PIRANHAPLANT_BBOX_WIDTH + 60
+			&& state == PIRANHAPLANT_STATE_INACTIVE && delay_start == 0
+			&& !isMarioInActiveZone)
+		{
+			SetState(PIRANHAPLANT_STATE_DARTING);
+			isMarioInActiveZone = true;
+		}
+		if (floor(mario->x) < x - 60 || floor(mario->x) > x + PIRANHAPLANT_BBOX_WIDTH + 60) {
+			isMarioInActiveZone = false;
+		}
+
+		/*if (mario->GetLevel() == MARIO_LEVEL_TAIL) {
+			mario->GetTail()->GetBoundingBox(mLeft, mTop, mRight, mBottom);
+
+			if (isColliding(floor(mLeft), mTop, ceil(mRight), mBottom) && mario->isTuring) {
+				mario->AddScore(x, y, 100);
+				SetState(PIRANHAPLANT_STATE_DEATH);
+				mario->GetTail()->ShowHitEffect();
+			}
+		}*/
+	}
+}
+
+
+void PiranhaPlantFire::GetBoundingBox(float& left, float& top,
+	float& right, float& bottom)
+{
+	left = x;
+	top = y;
+	right = x + PIRANHAPLANT_BBOX_WIDTH;
+	bottom = y + BBHeight;
+}
+
+void PiranhaPlantFire::SetState(int state) {
+	CGameObject::Update(state);
+	switch (state)
+	{
+	case PIRANHAPLANT_STATE_DARTING:
+		vy = -PIRANHAPLANT_DARTING_SPEED;
+		break;
+	case PIRANHAPLANT_STATE_SHOOTING:
+		vy = 0;
+		Shoot();
+		break;
+	case PIRANHAPLANT_STATE_DEATH:
+		vy = 0;
+		//SetType(IGNORE);
+		StartDying();
+		break;
+	case PIRANHAPLANT_STATE_INACTIVE:
+		vy = 0;
+		break;
+	}
+}
+
+void PiranhaPlantFire::GetDirect() {
+	CMario* mario = ((CPlayScene*)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	int mHeight;
+	if (mario->GetLevel() == MARIO_LEVEL_SMALL)
+		mHeight = MARIO_SMALL_BBOX_HEIGHT;
+	else
+		mHeight = MARIO_BIG_BBOX_HEIGHT;
+
+	if (mario->y + mHeight < limitY + BBHeight)
+		Up = true;
+	else
+		Up = false;
+	if (mario->x <= x)
+		Right = false;
+	else
+		Right = true;
+};
+
+void PiranhaPlantFire::Shoot() {
+	CPlayScene* currentScene = (CPlayScene*)CGame::GetInstance()->GetCurrentScene();
+	CAnimationSets* animation_sets = CAnimationSets::GetInstance();
+	this->bullet = new FireBullet(x, y, Up, Right);
+
+	//! Basic setup for bullet object
+	int ani_set_id = BULLET_ANI_SET_ID;
+	LPANIMATION_SET ani_set = animation_sets->Get(ani_set_id);
+	bullet->SetAnimationSet(ani_set);
+	currentScene->AddObject(bullet);
+}
---
+++ 05-SceneManager/PiranhaPlantFire.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "PiranhaPlant.h"
+#include "FireBullet.h"
+
+#define PIRANHAPLANT_GREEN_TYPE					0
+#define PIRANHAPLANT_RED_TYPE					1
+
+#define PIRANHAPLANT_BBOX_WIDTH					20
+#define PIRANHAPLANT_GREEN_BBOX_HEIGHT			24
+#define PIRANHAPLANT_RED_BBOX_HEIGHT			32
+#define PIRANHAPLANT_DARTING_SPEED				0.03f
+#define PIRANHAPLANT_BBOX_DEATH_WIDTH			16
+#define PIRANHAPLANT_BBOX_DEATH_HEIGHT			24
+
+#define PIRANHAPLANT_STATE_DARTING				0
+#define PIRANHAPLANT_STATE_SHOOTING				100
+#define PIRANHAPLANT_STATE_DEATH				200
+#define PIRANHAPLANT_STATE_INACTIVE				300
+
+#define PIRANHAPLANT_ANI_RIGHT_UP			0
+#define PIRANHAPLANT_ANI_RIGHT_DOWN			1
+#define PIRANHAPLANT_ANI_LEFT_UP			2
+#define PIRANHAPLANT_ANI_LEFT_DOWN			3
+#define PIRANHAPLANT_ANI_DEATH				4
+
+#define PIRANHAPLANT_DELAY_TIME			750
+#define PIRANHAPLANT_AIM_TIME			750
+#define PIRANHAPLANT_DIYING_TIME		250
+
+#define PIRANHAPLANT_ACTIVE_RANGE		20
+
+#define BULLET_ANI_SET_ID		9
+
+class PiranhaPlantFire :
+	public PiranhaPlant
+{
+	DWORD shooting_start = 0;
+	DWORD aim_start = 0;
+	DWORD delay_start = 0;
+	DWORD dying_start = 0;
+	float limitY = 0;
+	bool Up = false;
+	bool Right = false;
+	int BBHeight = 0;
+	bool isMarioInActiveZone = false;
+
+	FireBullet* bullet = NULL;
+
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* colliable_objects = NULL);
+	virtual void Render();
+public:
+
+	PiranhaPlantFire(int tag);
+	void GetDirect();
+	void StartShooting() { shooting_start = GetTickCount64(); }
+	void StartAim() { aim_start = GetTickCount64(); }
+	void StartDelay() { delay_start = GetTickCount64(); }
+	void StartDying() { dying_start = GetTickCount64(); }
+	void Shoot();
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void SetState(int state);
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+
+	void SetLimitY(float ly)
+	{
+		if (tag == 0)
+			BBHeight = PIRANHAPLANT_GREEN_BBOX_HEIGHT;
+		else BBHeight = PIRANHAPLANT_RED_BBOX_HEIGHT;
+		limitY = ly - BBHeight;
+	}
+};
